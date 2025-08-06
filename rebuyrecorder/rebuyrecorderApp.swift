abce21b14892af69450fa636acdf6b46166649b8
CzikSon18 <k.brudnicki@rebuy.com>
2025-08-06 15:55:48 +0200

    Initial Commit

---
+++ rebuyrecorder/rebuyrecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  rebuyrecorderApp.swift
+//  rebuyrecorder
+//
+//  Created by test on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct rebuyrecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
